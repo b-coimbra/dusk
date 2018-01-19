e92e3076ba96010d604ef00389b378c1708aaaae
Bruno Coimbra <b-coimbra@hotmail.com>
2018-01-19 20:13:06 -0200

    refactoring

--- js/script.js
+++ js/script.js
@@ -1,28 +1,39 @@
-const $ = (e) => document.querySelector(e);
+const $  = (e) => document.querySelector(e);
+const $A = (e) => document.querySelectorAll(e);
 
 (function () {
     'use strict';
 
-    // fetch random gif
-    for (var i = 1; i < 5; i++) {
-        var section   = Math.floor(Math.random() * 4) + 1;
-        var images    = Array.from({length: 5}, (e, i) => `section-${section}-${i + 1}`);
-        var randImage = images[Math.floor(Math.random() * images.length)];
-        var isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
-
-        $(`.\\5f nav section:nth-child(${section})`).style.setProperty("--img", `url("${isFirefox ? '../' : './'}img/${randImage}.gif")`);
+    function engines () {
+        return {
+            '!g': ['https://google.com/search?q=', 'Google'],
+            '!i': ['https://ixquick.com/do/search?q=', 'Ixquick'],
+            '!d': ['https://duckduckgo.com/html?q=', 'DuckDuckGo'],
+            '!y': ['https://youtube.com/results?search_query=', 'Youtube'],
+            '!w': ['https://en.wikipedia.org/w/index.php?search=', 'Wikipedia']
+        };
     }
 
-    // search mechanism
+    // +--------+
+    // | IMAGES |
+    // +--------+
+    setInterval(() => {
+        for (var i = 1; i < 5; i++) {
+            var section   = Math.floor(Math.random() * 4) + 1;
+            var images    = Array.from({length: 5}, (e, i) => `section-${section}-${i + 1}`);
+            var randImage = images[Math.floor(Math.random() * images.length)];
+            var isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+
+            $(`.\\5f nav section:nth-child(${section})`).style.setProperty("--img", `url("${isFirefox ? '../' : './'}img/${randImage}.gif")`);
+        }
+    }, 5000);
+
+    // +--------+
+    // | SEARCH |
+    // +--------+
     var search  = $('._search');
     var input   = $('._search input[type="text"]');
-    var engines = {
-        '!g': ['https://google.com/search?q=', 'Google'],
-        '!i': ['https://ixquick.com/do/search?q=', 'Ixquick'],
-        '!d': ['https://duckduckgo.com/html?q=', 'DuckDuckGo'],
-        '!y': ['https://youtube.com/results?search_query=', 'Youtube'],
-        '!w': ['https://en.wikipedia.org/w/index.php?search=', 'Wikipedia']
-    };
+    var engines = engines();
 
     for (var key in engines)
         $('.search-engines').innerHTML += `<li><p title="${engines[key][1]}">${key}</p></li>`;
@@ -40,7 +51,7 @@ const $ = (e) => document.querySelector(e);
             var engine = engines['!g'][0]; // default engine
             var str    = 0;
 
-            document.querySelectorAll('.search-engines li p').forEach((eng) => {
+            $A('.search-engines li p').forEach((eng) => {
                 var current = eng.parentNode;
 
                 (prefix == eng.innerHTML)
@@ -52,21 +63,21 @@ const $ = (e) => document.querySelector(e);
                 if (prefix.indexOf('!') == 0)
                     (engine = engines[prefix][0], str = 3);
 
-                window.location = engine + args.join(' ').substr(str);
+                window.location = engine + args.join(' ').substr(str).toString().replace(/\s+/m, '%20');
             }
         });
     };
 
-    // clock
+    // +-------+
+    // | CLOCK |
+    // +-------+
     var date  = new Date();
-    var hours = date.getHours();
     var month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     var days  = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-    let setTime = (div, time) => { div.innerHTML += time };
+    let setTime = (div, time) => { div.innerHTML += time; };
 
     setTime($('.day'),  days[date.getDay()].substr(0, 3));
-    setTime($('.hour'), hours + '\'');
+    setTime($('.hour'), date.getHours() + '\'');
     setTime($('.mins'), date.getMinutes());
 })();
-
