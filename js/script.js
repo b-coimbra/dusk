617dee7fa525b37f848ac6e6690994f94f24ca93
Bruno Coimbra <b-coimbra@hotmail.com>
2018-01-23 19:43:34 -0200

    refactoring - removed redundant characters

--- js/script.js
+++ js/script.js
@@ -19,10 +19,10 @@ const $A = (e) => document.querySelectorAll(e);
     // +--------+
     setInterval(() => {
         for (var i = 1; i < 5; i++) {
-            var section   = Math.floor(Math.random() * 4) + 1;
-            var images    = Array.from({length: 5}, (e, i) => `section-${section}-${i + 1}`);
-            var randImage = images[Math.floor(Math.random() * images.length)];
-            var isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+            let section   = Math.floor(Math.random() * 4) + 1,
+                images    = Array.from({length: 5}, (e, i) => `section-${section}-${i + 1}`),
+                randImage = images[Math.floor(Math.random() * images.length)],
+                isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
 
             $(`.\\5f nav section:nth-child(${section})`).style.setProperty("--img", `url("${isFirefox ? '../' : './'}img/${randImage}.gif")`);
         }
@@ -31,9 +31,9 @@ const $A = (e) => document.querySelectorAll(e);
     // +--------+
     // | SEARCH |
     // +--------+
-    var search  = $('._search');
-    var input   = $('._search input[type="text"]');
-    var engines = engines();
+    var search  = $('._search'),
+        input   = $('._search input[type="text"]'),
+        engines = engines();
 
     for (var key in engines)
         $('.search-engines').innerHTML += `<li><p title="${engines[key][1]}">${key}</p></li>`;
@@ -46,13 +46,13 @@ const $A = (e) => document.querySelectorAll(e);
         input.scrollIntoView();
 
         search.addEventListener("keyup", (e) => {
-            var args   = e.target.value.split(' ');
-            var prefix = args[0];
-            var engine = engines['!g'][0]; // default engine
-            var str    = 0;
+            let args   = e.target.value.split(' '),
+                prefix = args[0],
+                engine = engines['!g'][0], // default engine
+                str    = 0;
 
             $A('.search-engines li p').forEach((eng) => {
-                var current = eng.parentNode;
+                let current = eng.parentNode;
 
                 (prefix == eng.innerHTML)
                     ? current.classList.add('active')
@@ -71,9 +71,9 @@ const $A = (e) => document.querySelectorAll(e);
     // +-------+
     // | CLOCK |
     // +-------+
-    var date  = new Date();
-    var month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    var days  = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    let date  = new Date(),
+        month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        days  = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
     let setTime = (div, time) => { div.innerHTML += time; };
 
